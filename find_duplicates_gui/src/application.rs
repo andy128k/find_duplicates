d37788550a9b1f9630eb3f5777d070670aeeb5e2
Andrey Kutejko <andy128k@gmail.com>
2020-07-05 23:55:01 +0200

    use gtk::ApplicationBuilder to create an app instance

--- find_duplicates_gui/src/application.rs
+++ find_duplicates_gui/src/application.rs
@@ -2,16 +2,10 @@ use crate::main_window::MainWindow;
 use gio::prelude::*;
 
 pub fn create_application() -> gtk::Application {
-    let app = gtk::Application::new(
-        Some("net.andy128k.FindDuplicates"),
-        gio::ApplicationFlags::FLAGS_NONE,
-    )
-    .unwrap();
-
-    // let app = gtk::ApplicationBuilder::new()
-    //     .application_id("net.andy128k.FindDuplicates")
-    //     .flags(gio::ApplicationFlags::FLAGS_NONE)
-    //     .build();
+    let app = gtk::ApplicationBuilder::new()
+        .application_id("net.andy128k.FindDuplicates")
+        .flags(gio::ApplicationFlags::FLAGS_NONE)
+        .build();
 
     app.connect_activate(|app| {
         let app_window = MainWindow::new(app);
